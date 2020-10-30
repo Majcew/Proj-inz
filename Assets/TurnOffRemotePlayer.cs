b25afdf0272b4ed88343ddb054ba662fd306c1f2
Szymek <szymonjoszko@gmail.com>
2020-10-31 00:18:59 +0100

    bugfix camery w trybie multi

--- Assets/TurnOffRemotePlayer.cs
+++ Assets/TurnOffRemotePlayer.cs
@@ -9,15 +9,18 @@ public class TurnOffRemotePlayer : NetworkBehaviour
     {
         string id = string.Format("{0}", this.netId);
         Player_Movement scr = this.GetComponent<Player_Movement>();
+        Camera cam = this.GetComponentInChildren< Camera >();
 
         if (this.isLocalPlayer)
         {
+            cam.enabled = true;
             scr.enabled = true;
             Cursor.visible = true;
             Cursor.lockState = CursorLockMode.Locked;
         }
         else
         {
+            cam.enabled = false;
             scr.enabled = false;
             Cursor.visible = false;
             Cursor.lockState = CursorLockMode.Locked;
