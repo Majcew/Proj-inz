7399e35b8e0dec738dda1d42d267f96e937ac380
Majcew <48514316+Majcew@users.noreply.github.com>
2020-11-01 21:25:05 +0100

    dodałem drabinę oraz nowe pomieszczenie w scenie Tutorial, skrypt do drabiny nie działa poprawnie

---
+++ Assets/Scripts/ClimbLadder.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System;
+
+public class ClimbLadder : MonoBehaviour
+{
+    #region Variables
+    public GameObject playerOBJ;
+    public bool canClimb = false;
+    public float speed = 1;
+    #endregion
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            canClimb = true;
+            playerOBJ = other.gameObject;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            canClimb = false;
+            playerOBJ = null;
+        }
+    }
+    void Update()
+    {
+        if (canClimb)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                playerOBJ.transform.Translate(Vector3.up * Time.deltaTime * speed);
+                Console.WriteLine(Vector3.up * Time.deltaTime * speed);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                playerOBJ.transform.Translate(Vector3.down * Time.deltaTime * speed);
+                Console.WriteLine(Vector3.down * Time.deltaTime * speed);
+            }
+        }
+    }
+}
