c4440fd9cf62ff5660d52dc1e8c2f14a67586b3f
Majcew <48514316+Majcew@users.noreply.github.com>
2020-11-07 15:27:42 +0100

    Dodałem proste UI jako menu główne oraz pole do wpisywania IP

---
+++ Assets/MenuBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuBehaviour : MonoBehaviour
+{
+    #region Variables
+    private string _IP;
+    private Text IP_text;
+    #endregion
+
+    private void Start()
+    {
+        IP_text = GameObject.Find("IP").GetComponent<Text>();
+    }
+    public void Firstlvl()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Secondlvl()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Thirdlvl()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void OnTextChange(string text) {
+        _IP = text;
+        IP_text.text = _IP;
+    }
+    
+}
