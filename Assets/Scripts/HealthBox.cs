fcc989566d8f94d687b9e2a0144e21389c0a5da6
Majcew <48514316+Majcew@users.noreply.github.com>
2020-12-10 11:20:13 +0100

    Dodałem skrypty dla apteczki i amunicji

---
+++ Assets/Scripts/HealthBox.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBox : MonoBehaviour
+{
+    [SerializeField]
+    private int _restoreHealth = 20;
+    [HideInInspector]
+    public int restoreHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        restoreHealth = _restoreHealth;
+    }
+}
--- Assets/Scripts/PickupObject.cs
+++ Assets/Scripts/PickupObject.cs
@@ -5,23 +5,51 @@ public class PickupObject : MonoBehaviour
 {
     [SerializeField]
     private GameObject pickupText;
+    private GameObject collidingItem;
 
    void Update() {
-        if (Input.GetKeyDown("e"))
+        if (Input.GetKeyDown("f") && collidingItem != null)
         {
-            // ObjectIwantToPickUp.GetComponent<Transform>();
+            switch (collidingItem.tag)
+            {
+                case "health":
+                    //CmdAddHealth();
+                    AddHealth(collidingItem.GetComponent<HealthBox>().restoreHealth);
+                    break;
+                case "ammunition":
+                    //CmdAddAmmo();
+                    AddAmmo(collidingItem.GetComponent<PickupableAmmo>().index, collidingItem.GetComponent<PickupableAmmo>().amount);
+                    break;
+            }
         }
     }
-    private void OnTriggerEnter(Collider other) // to see when the player enters the collider
+    private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("health") || other.gameObject.CompareTag("ammunition")) //on the object you want to pick up set the tag to be anything, in this case "object"
-        {
-            //TODO: Add script to add health or ammunition to the player
-            pickupText.SetActive(true);
-        }
+        collidingItem = other.gameObject;
+        pickupText.SetActive(true);
+        
     }
     private void OnTriggerExit(Collider other)
     {
+        collidingItem = null;
+        pickupText.SetActive(false);
+    }
+
+    void AddHealth(int amount)
+    {
+        Health player_health = GetComponent<Health>();
+        bool active = player_health.RestoreHP(amount);
+        if (!active) { 
+            Destroy(collidingItem);
+            pickupText.SetActive(false);
+        }
+    }
+
+    void AddAmmo(int index,int amount)
+    {
+        Ammunition player_ammunition = GetComponent<Ammunition>();
+        player_ammunition.AddAmmunition(index, amount);
+        Destroy(collidingItem);
         pickupText.SetActive(false);
     }
 }
