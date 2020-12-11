994dbeb6e5afcdbbaf069d786579fd5405aa986d
Szymek <szymonjoszko@gmail.com>
2020-12-11 20:58:02 +0100

    Bugfix zabijania zoombie przez clienta
     - dodanie więcej zoombie do mapy pvp
     - aktualizacja EnemyHealth
     - aktualizacja GameManager
       - dodanie dictionary przechowywujące EnemyHealth
     - aktualizacja Shoot

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     private static Dictionary<string, Health> playerHealths = new Dictionary<string, Health>();
+    private static Dictionary<string, EnemyHealth> enemyHealths = new Dictionary<string, EnemyHealth>();
 
     public static void AddPlayerHealth (string netId, Health player_health)
     {
@@ -12,6 +13,14 @@ public class GameManager : MonoBehaviour
         player_health.transform.name = name;
         playerHealths.Add(netId, player_health);
         Debug.Log("PlayerHealth of " + name + " added");
+    }
+
+    public static void AddEnemyHealth(string netId, EnemyHealth enemy_health)
+    {
+        string name = "Enemy" + netId;
+        enemy_health.transform.name = name;
+        enemyHealths.Add(netId, enemy_health);
+        Debug.Log("EnemyHealth of " + name + " added");
     }
 
     public static void RemovePlayerHealth(string netId)
@@ -23,5 +32,10 @@ public class GameManager : MonoBehaviour
     public static Health GetPlayerHealth(string netId)
     {
         return playerHealths[netId];
+    }
+
+    public static EnemyHealth GetEnemyHealth(string netId)
+    {
+        return enemyHealths[netId];
     }
 }
--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -15,6 +15,7 @@ public class Shoot : NetworkBehaviour
     public AudioSource[] audiosources;
     public AudioClip[] shootingsounds;
     public ParticleSystem[] muzzleParticle;
+    enum HitTarget { Player, Enemy };
     private void Start()
     {
         n_animator = GetComponent<NetworkAnimator>();
@@ -75,12 +76,16 @@ public class Shoot : NetworkBehaviour
                         NetworkIdentity netIdentity = hit.transform.GetComponent<NetworkIdentity>();
                         if (netIdentity != null)
                         {
-                            CmdShootBullet(netIdentity.netId.ToString(), damages[id]);
-                        }
-                        EnemyHealth enemy = hit.transform.GetComponent<EnemyHealth>();
-                        if (enemy != null)
-                        {
-                            enemy.TakeDamage(damages[id]);
+                            HitTarget hitTarget;
+                            if (hit.transform.GetComponent<EnemyHealth>() == null)
+                            {
+                                hitTarget = HitTarget.Player;
+                            }
+                            else
+                            {
+                                hitTarget = HitTarget.Enemy;
+                            }
+                            CmdShootBullet(netIdentity.netId.ToString(), damages[id], hitTarget);
                         }
                     }
                     n_animator.SetTrigger("Sword_attack");
@@ -88,7 +93,7 @@ public class Shoot : NetworkBehaviour
                 case 3: // Shotgun
                     n_animator.SetTrigger("Shoot");
                     for (int i = 0; i < 8; i++)
-                    {
+                    { 
                         Vector3 spread = new Vector3();
                         Vector3 direction = fpscam.transform.forward;
                         spread += fpscam.transform.up * Random.Range(-1.5f, 1.5f);
@@ -100,14 +105,17 @@ public class Shoot : NetworkBehaviour
                             NetworkIdentity netIdentity = hit.transform.GetComponent<NetworkIdentity>();
                             if (netIdentity != null)
                             {
-                               CmdShootBullet(netIdentity.netId.ToString(), damages[id]);
+                                HitTarget hitTarget;
+                                if (hit.transform.GetComponent<EnemyHealth>() == null)
+                                {
+                                    hitTarget = HitTarget.Player;
+                                }
+                                else
+                                {
+                                    hitTarget = HitTarget.Enemy;
+                                }
+                                CmdShootBullet(netIdentity.netId.ToString(), damages[id], hitTarget);
                             }
-                            EnemyHealth enemy = hit.transform.GetComponent<EnemyHealth>();
-                            if (enemy != null)
-                            {
-                                enemy.TakeDamage(damages[id]);
-                            }
-
                         }
                         else Debug.DrawRay(fpscam.transform.position, direction, Color.red, 3.0f);
                     }
@@ -117,14 +125,18 @@ public class Shoot : NetworkBehaviour
                     if (Physics.Raycast(fpscam.transform.position, fpscam.transform.forward, out hit))
                     {
                         NetworkIdentity netIdentity = hit.transform.GetComponent<NetworkIdentity>();
-                        if (netIdentity != null && hit.transform.CompareTag("Player"))
+                        if (netIdentity != null)
                         {
-                           CmdShootBullet(netIdentity.netId.ToString(), damages[id]);
-                        }
-                        EnemyHealth enemy = hit.transform.GetComponent<EnemyHealth>();
-                        if (enemy != null && hit.transform.CompareTag("zombie"))
-                        {
-                            enemy.TakeDamage(damages[id]);
+                            HitTarget hitTarget;
+                            if (hit.transform.GetComponent<EnemyHealth>() == null)
+                            {
+                                hitTarget = HitTarget.Player;
+                            }
+                            else
+                            {
+                                hitTarget = HitTarget.Enemy;
+                            }
+                            CmdShootBullet(netIdentity.netId.ToString(), damages[id], hitTarget);
                         }
                     }
                     break;
@@ -135,10 +147,28 @@ public class Shoot : NetworkBehaviour
     }
 
     [Command]
-    private void CmdShootBullet(string netId, float damage)
+    private void CmdShootBullet(string netId, float damage, HitTarget hitTarget)
     {
         Debug.Log("CmdShootBullet: netId: " + netId + " damage: " + damage);
-        GameManager.GetPlayerHealth(netId).RcpTakeDamage(damage);
+        switch (hitTarget)
+        {
+            case HitTarget.Enemy: 
+                {
+                    GameManager.GetEnemyHealth(netId).TakeDamage(damage);
+                    break;
+                }
+            case HitTarget.Player:
+                {
+                    GameManager.GetPlayerHealth(netId).RcpTakeDamage(damage);
+                    break;
+                }
+            default:
+                {
+                    Debug.Log("No method for this target");
+                    break;
+                }
+
+        }
     }
 
     private void ReloadMag()
