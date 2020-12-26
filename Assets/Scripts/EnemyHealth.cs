0ca6939e0190fb96a175aa1de364d988825ec536
Majcew <48514316+Majcew@users.noreply.github.com>
2020-12-26 18:20:39 +0100

    Dodałem system dropienia przedmiotów podczas śmierci potworów, collider itemków lekko poprawiony

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -7,19 +7,24 @@ public class EnemyHealth : NetworkBehaviour
     public float startingHealth = 40f;
     [SyncVar]
     public float currentHealth;
-    public float sinkSpeed = 2.5f; //szybkość znikania w podłodze
-    //public int scoreValue = 10;
-    //nie wiem, czy będziemy dawać punkty, ale to na razie zostawię
+    //public int scoreValue = 10; nie wiem, czy będziemy dawać punkty, ale to na razie zostawię
     public AudioClip deathClip;
 
     Animator anim;
     NetworkAnimator n_anim;
     UnityEngine.AI.NavMeshAgent nav;
     AudioSource enemyAudio;
-    //ParticleSystem hitParticles;
-    //do efektów krwi przy trafieniu, nie ruszałem
+    //ParticleSystem hitParticles; do efektów krwi przy trafieniu, nie zaimplementowane
     CapsuleCollider capsuleCollider;
     bool isDead;
+
+    [Header("List of items that the enemy can drop after death")]
+    [SerializeField]
+    private GameObject[] item_list;
+
+    [Header("List of behaviours to disable after death")]
+    [SerializeField]
+    private Behaviour[] behaviours;
     void Awake()
     {
         anim = GetComponent<Animator>();
@@ -48,6 +53,7 @@ public class EnemyHealth : NetworkBehaviour
         {
             nav.enabled = false;
             DisableAllScripts();
+            RollItem();
             Death();
         }
     }
@@ -73,16 +79,27 @@ public class EnemyHealth : NetworkBehaviour
     public void StartSinking(){
         Destroy(gameObject, 6f);
     }
+    /* Funkcja odpowiedzialna za spawnowanie różnych itemków po śmierci przeciwnika.
+       W celu poprawnego działania przedmioty powinny posiadać odpowiednie skrypty od pakietu "Mirror".
+       Należy te obiekty także podpiąć do "NetworkManagera" w zakładce "Spawnable Prefabs"
+    */
+    private void RollItem()
+    {
+        // Pobieramy pozycję przeciwnika po śmierci
+        Vector3 position = transform.position;
+        // Będziemy losowali z tablicy o zadanej wymiarze.
+        int index = Random.Range(0,item_list.Length);
+        if(item_list[index] != null)
+        {
+            GameObject toRender = Instantiate(item_list[index], position + new Vector3(0.0f, 0.3f, 0.0f), Quaternion.identity);
+            NetworkServer.Spawn(toRender);     
+        }
+    }
 
     private void DisableAllScripts()
     {
-        MonoBehaviour[] comps = GetComponents<MonoBehaviour>();
-        NetworkBehaviour[] comps2 = GetComponents<NetworkBehaviour>();
-        foreach (MonoBehaviour c in comps)
-        {
-            c.enabled = false;
-        }
-        foreach (NetworkBehaviour c in comps2)
+        gameObject.GetComponent<CapsuleCollider>().enabled = false;
+        foreach (Behaviour c in behaviours)
         {
             c.enabled = false;
         }
