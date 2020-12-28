f5cdd2244161fe5f98f09240174a78b5351ebca3
Majcew <48514316+Majcew@users.noreply.github.com>
2020-12-28 19:09:02 +0100

    Dodałem działający system dropienia itemków od potworów

---
+++ Assets/Scripts/ServerSpawner.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using Mirror;
+
+public class ServerSpawner : NetworkBehaviour
+{
+    public static void SpawnObject(GameObject obj)
+    {
+        CmdSpawnObject(obj);
+    }
+    private static void CmdSpawnObject(GameObject obj)
+    {
+        Debug.Log("spawn");
+        NetworkServer.Spawn(obj);
+    }
+}
