cc5672d7dd6634a7f6c7f146707af53a113314a2
Majcew <48514316+Majcew@users.noreply.github.com>
2020-12-29 18:31:56 +0100

    Dodałem spawner potworków

--- Assets/Scripts/ServerSpawner.cs
+++ Assets/Scripts/ServerSpawner.cs
@@ -3,19 +3,38 @@ using Mirror;
 
 public class ServerSpawner : NetworkBehaviour
 {
-    /*public static void SpawnObject(GameObject obj)
+    [SerializeField]
+    private float timeBetweenSpawns;
+    [SerializeField]
+    private GameObject[] prefabs;
+    [SerializeField]
+    private float y_offset;
+    private Vector3 position;
+    private float timer;
+
+    private void Start()
     {
-        CmdSpawnObject(obj);
+        position = gameObject.transform.position;
     }
-    [Command]
-    private static void CmdSpawnObject(GameObject obj)
+    private void Update()
     {
-        RpcSpawnObject(obj);
+       timer += Time.deltaTime;
+       if(timer > timeBetweenSpawns)
+        {
+            CmdSpawnPrefab();
+            timer = 0f;
+        }
+    }
+    [Command(ignoreAuthority = true)]
+    private void CmdSpawnPrefab()
+    {
+        int index = Random.Range(0, prefabs.Length);
+        RpcSpawnPrefab(index);
     }
     [ClientRpc]
-    private static void RpcSpawnObject(GameObject obj)
+    private void RpcSpawnPrefab(int index)
     {
-        Debug.Log("spawn");
-        NetworkServer.Spawn(obj);
-    }*/
+        GameObject toRender = Instantiate(prefabs[index], position + new Vector3(0,y_offset,0), Quaternion.identity);
+        NetworkServer.Spawn(toRender);
+    }
 }
