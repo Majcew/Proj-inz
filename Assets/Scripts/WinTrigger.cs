330634eabec44e90f6c61475c657b71fa85a010c
Szymek <szymonjoszko@gmail.com>
2020-12-30 21:17:19 +0100

    Zakończenie v1

---
+++ Assets/Scripts/WinTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using UnityEngine.SceneManagement;
+
+public class WinTrigger : NetworkBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("you win");
+            PlayerPrefs.SetInt("exit_scene", 4);
+            GameObject.Find("NetworkManager").GetComponent<NetworkManager>().StopClient();
+            GameObject.Find("NetworkManager").GetComponent<NetworkManager>().StopHost();
+            PlayerPrefs.DeleteKey("exit_scene");
+            SceneManager.LoadScene(4);
+        }
+    }
+
+  
+}
